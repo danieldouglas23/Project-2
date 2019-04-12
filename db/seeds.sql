7bfab1c6ea5e46391e2382c57127aca6d70e867e
Matthew Hopkins <mattrhopkins@gmail.com>
2019-04-12 14:57:44 -0600

    added db files

---
+++ db/seeds.sql
@@ -0,0 +1,66 @@
+SELECT * FROM recipes
+
+INSERT INTO recipes
+    (author, title, ingredients, preparation, image_link, hands_on_time, total_time, servings)
+VALUES
+    ('Matt Hopkins',
+        'Steak Picadillo Soft Tacos',
+        "1 tablespoon extra-virgin olive oil 
+|| 1 12-ounce strip of skirt steak 
+|| 1/2 cup 1/4-inch squares green bell pepper 
+|| 3/4 cup canned diced tomatoes with green chiles
+|| 1/3 cup halved drained pimiento-stuffed green olives plus brine from jar 
+|| 1/4 cup raisins 
+|| 1 tablespoon tomato paste 
+|| 1 teaspoon ground cumin 
+|| 1/2 teaspoon ground allspice 
+|| 8 corn tortillas
+|| Chopped fresh cilantro (for garnish)", 
+"Heat oil in large nonstick skillet over medium-high heat. Sprinkle steak with salt and pepper. Add to skillet; sauté 3 to 4 minutes per side for medium-rare. Transfer to plate. Add bell pepper to skillet. Sauté 2 minutes. Add tomatoes with green chiles,olives, raisins, tomato paste, cumin, and allspice. Simmer until sauce is thick, stirring often, 3 to 4 minutes. Season to taste with salt, pepper, and olive brine.
+Cut steak into 1/3-inch-thick slices on diagonal across grain. Add steak and any juices to sauce in skillet; toss to blend.
+Char tortillas over gas flame or in broiler until blackened in spots, 15 to 20 seconds per side. Divide filling among tortillas. Sprinkle with cilantro and serve.", 
+        'https://assets.epicurious.com/photos/54b2f0a711c5d6af478b43ec/6:4/w_620%2Ch_413/364552_steak-picadillo-soft-tacos_1x1.jpg',
+        35,
+        35,
+        4);
+
+INSERT INTO recipes
+    (author, title, ingredients, preparation, image_link, hands_on_time, total_time, servings)
+VALUES
+    ('Michael Diaz',
+        'Tacos al Pastor',
+"1 large white onion halved 
+|| 1 pineapple peeled cut crosswise into 1/2-inch-thick rounds 
+|| 1/2 cup fresh orange juice 
+|| 1/4 cup distilled white vinegar 
+|| 1/4 cup guajillo chile powder 
+|| 3 garlic cloves halved 
+|| 2 teaspoons coarse kosher salt 
+|| 1 teaspoon dried oregano (preferably Mexican) 
+|| 1 teaspoon ground cumin 
+|| 1 large or 2 small chipotle chiles and 1 to 2 teaspoons adobo from canned chipotle chiles in adobo 
+|| 1 2 1/2-to 3-pound boneless pork loin cut into 1/2-inch slices 
+|| 1/4 cup chopped fresh cilantro 
+|| Corn tortillas 
+|| Smoky Two-Chile Salsa 
+|| Lime wedges", 
+"Coarsely chop 1 onion half. Coarsely chop 2 pineapple rounds, discarding core; cover and chill remaining pineapple. Place chopped onion and chopped pineapple in blender. Add orange juice and next 7 ingredients; puree marinade until smooth. Place pork in large resealable plastic bag. Add marinade and seal bag, releasing excess air. Turn to coat. Chill at least 4 hours and up to 1 day.
+Prepare barbecue (medium-high heat). Grill remaining pineapple until warm and slightly charred, 4 to 6 minutes per side. Grill pork with some marinade still clinging until slightly charred and cooked through, 2 to 4 minutes per side. Transfer pineapple and pork to work surface; chop pineapple into 1/2-inch cubes, discarding cores. Chop pork. Transfer to platter; toss to combine.
+Meanwhile, finely chop remaining onion half and place in medium bowl. Add cilantro; toss to combine. Grill tortillas until warm and slightly charred, about 10 seconds per side.
+Serve pork-pineapple mixture with onion-cilantro relish, Smoky Two-Chile Salsa, and lime wedges.
+Test Kitchen Tip: To make your own guajillo chile powder, finely grind about 6 large dried seeded guajillo chiles in a spice mill to yield about 1/4 cup powder.", 
+        'https://assets.epicurious.com/photos/5b747e86fb8a0329e97a82bb/6:4/w_620%2Ch_413/tacos-al-pasto-recipe-081518.jpg',
+        35,
+        1440,
+        6-8;
+
+
+
+
+
+
+
+
+
+
+
